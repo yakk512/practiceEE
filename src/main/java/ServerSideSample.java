2322dd6ea9b151c6a64c0cee1afce9d5d81e73e5
Keita Yamada <yshiro9@yahoo.co.jp>
2015-07-26 17:59:28 +0900

    サーバサイドでの入出力を処理クラスServerSideSample.classを追加

---
+++ src/main/java/ServerSideSample.java
@@ -0,0 +1,31 @@
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author yackman
+ */
+@Named
+@RequestScoped
+public class ServerSideSample {
+    
+    private String inputName;
+    
+    public String getHoge()
+    {
+        return inputName;
+    }
+        
+    public void setHoge(String input)
+    {
+        inputName = input;
+    }
+    
+}
